898d16f9ed3b5a44287607e7a924789ee1e64ec2
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-08-04 16:55:12 +0700

    Add division function
    Fix always zero result from mmultiplication function

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ function substraction(...numbers) {
 }
 
 function multiplication(...numbers) {
-    let sum = 0;
+    let sum = 1;
     for (const number of numbers) {
         sum *= number;
     }
     
     return sum;
 }
+
+function division(...numbers) {
+    let sum = 1;
+    for (const number of numbers) {
+        sum /= number;
+    }
+    
+    return sum;
+}
+
+console.log(division(3, 2))
