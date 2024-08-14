e1bbd7b7def89d84b1726cd07b17e12b56ee1858
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-08-14 21:09:52 +0700

    Evaluate calculation after second operator call

--- index.js
+++ index.js
@@ -41,33 +41,80 @@ function evaluate(operand1, operator, operand2) {
     return sum;
 }
 
-function setUpButtons(input = "") {
+function setUpButtons() {
     let calcButtons = document.querySelectorAll("li");
+    let btnOperators = document.querySelectorAll(".calculation li")
     const display = document.querySelector(".container > #display");
 
     calcButtons.forEach((element) => {
         element.addEventListener("click", (e) => {
-            input += e.target.id;
-            display.textContent = input;
+            calculatorConfigs.input += e.target.id;
+            display.textContent = calculatorConfigs.input;
         })
     })
+
+    btnOperators.forEach((element) => {
+        element.addEventListener("click", (e) => {
+            if (calculatorConfigs.operand1 === null) {
+                calculatorConfigs.operand1 = +calculatorConfigs.input.slice(0, calculatorConfigs.input.length-1);
+                calculatorConfigs.operatorIndex = calculatorConfigs.input.length - 1;
+                calculatorConfigs.operator = e.target.id;
+            }
+
+            if (calculatorConfigs.operand2 === null && calculatorConfigs.operand1) {
+                console.log("update!")
+                calculatorConfigs.operand2 = +calculatorConfigs.input.slice(calculatorConfigs.operatorIndex, calculatorConfigs.input.length-1);
+                calculatorConfigs.sum = evaluate(calculatorConfigs.operand1, calculatorConfigs.operator, calculatorConfigs.operand2);
+
+                const newInput = calculatorConfigs.sum.toString() + calculatorConfigs.operator;
+                
+                calculatorConfigs.input = newInput;
+                calculatorConfigs.operand1 = calculatorConfigs.sum;
+                calculatorConfigs.operator = e.target.id;
+                calculatorConfigs.operatorIndex = calculatorConfigs.input.length - 1;
+                calculatorConfigs.operand2 = null;
+
+                display.textContent = calculatorConfigs.input;
+
+                console.log(calculatorConfigs)
+            }
+        })
+    })
+
 }
 
-function spreadOperandsAndOperators(input) {
-    let obj = {}
-    let operands = [...input.matchAll(/[0-9]+/g)];
-    let operators = [...input.matchAll(/[\+-\/*]/g)];
+// function spreadOperandsAndOperators(input) {
+//     let obj = {}
+//     let operands = [...input.matchAll(/[0-9]+/g)];
+//     let operators = [...input.matchAll(/[\+-\/*]/g)];
 
-    obj.operands = operands;
-    obj.operators = operators;
+//     obj.operands = operands;
+//     obj.operators = operators;
 
-    return obj;
+//     return obj;
+// }
+
+function resetCalculatorConfigs() {
+    calculatorConfigs.operand1 = null;
+    calculatorConfigs.operand2 = null;
+    calculatorConfigs.operator = null;
+    calculatorConfigs.sum = null;
+    calculatorConfigs.input = null;
 }
 
 function main() {
-    let input = "";
-    let operand1, operand2, operator, sum = null;
-    setUpButtons(input);
+    calculatorConfigs.input = "";
+    setUpButtons();
+}
+
+
+const calculatorConfigs = {
+    operand1: null,
+    operand2: null,
+    operator: null,
+    operatorIndex: 0,
+    sum: null,
+    input: null,
 }
 
 main();
