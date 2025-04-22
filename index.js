2582bff51448dbf41a95ad8f1b71e9b70e2f6758
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2025-04-22 10:25:41 +0700

    feat: Add new feature to index.js
    Four new features added, that is add, substract,
    multiply, and divide.

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  if (number2 === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return number1 / number2;
+}
+
