8e788a90256970e343b146e338fce34e1b1900d5
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2025-04-22 10:32:19 +0700

    feat: Add operate to accommodate calculation

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
+let number1 = 10;
+let number2 = 5;
+let operation = "add";
+
+function operate(number1, number2, operation) {
+  switch (operation) {
+    case "add":
+      return add(number1, number2);
+    case "subtract":
+      return subtract(number1, number2);
+    case "multiply":
+      return multiply(number1, number2);
+    case "divide":
+      return divide(number1, number2);
+    default:
+      throw new Error("Invalid operation");
+  }
+}
